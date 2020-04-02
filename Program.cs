381320d48110f60ea2fca2c8ac52f150f5197751
romak <romakrutsik@yahoo.com>
2020-04-02 18:50:47 +0300

    first

--- Program.cs
+++ Program.cs
@@ -9,46 +9,46 @@ namespace krlab3._0._1
     {
         static void Main(string[] args)
         {
-            ////Zavdannya 1
-            //List<int> listOfNumbers = new List<int>();
-            //int i = 0;
-            //string path = @"C:\Users\romak\Desktop\TestLab3.txt";
-            //using (StreamReader read = new StreamReader(path))
-            //{
-            //    while(read.Peek() > -1)
-            //    {
-            //        string s = read.ReadLine();
-            //        listOfNumbers.Add(s.Length);
-            //        Console.WriteLine(listOfNumbers[i]);
-            //        i++;
-            //    }
-            //}
-            ////Zavdannya 2
-            //Dictionary<int, string> dict = new Dictionary<int, string>();
-            
-            //while (Console.ReadKey(true).Key != ConsoleKey.Escape)
-            //{
-            //    Console.WriteLine("Enter the key(integer value)first, then the value itself(string value)");
-            //    Console.WriteLine("If you want to quit, press Esc");
-            //    int key = Convert.ToInt32(Console.ReadLine());
-            //    string value = Console.ReadLine();
-                
-            //    dict.Add(key,value);
-            //}
-            //int keyCompared = 100000;
-            //Console.WriteLine("Enter the key you want to compare with");
-            //int keyEntered = Convert.ToInt32(Console.ReadLine());
-            //foreach(KeyValuePair<int, string> keyValue in dict)
-            //{
-            //    if (keyValue.Key >= keyEntered)
-            //    {
-            //        if(keyValue.Key < keyCompared)
-            //        {
-            //            keyCompared = keyValue.Key;
-            //        }
-            //    }
-            //}
-            //Console.WriteLine("The closest key value to your entered is: " + keyCompared);
+            //Zavdannya 1
+            List<int> listOfNumbers = new List<int>();
+            int i = 0;
+            string path = @"C:\Users\romak\Desktop\TestLab3.txt";
+            using (StreamReader read = new StreamReader(path))
+            {
+                while (read.Peek() > -1)
+                {
+                    string s = read.ReadLine();
+                    listOfNumbers.Add(s.Length);
+                    Console.WriteLine(listOfNumbers[i]);
+                    i++;
+                }
+            }
+            //Zavdannya 2
+            Dictionary<int, string> dict = new Dictionary<int, string>();
+
+            while (Console.ReadKey(true).Key != ConsoleKey.Escape)
+            {
+                Console.WriteLine("Enter the key(integer value)first, then the value itself(string value)");
+                Console.WriteLine("If you want to quit, press Esc");
+                int key = Convert.ToInt32(Console.ReadLine());
+                string value = Console.ReadLine();
+
+                dict.Add(key, value);
+            }
+            int keyCompared = 100000;
+            Console.WriteLine("Enter the key you want to compare with");
+            int keyEntered = Convert.ToInt32(Console.ReadLine());
+            foreach (KeyValuePair<int, string> keyValue in dict)
+            {
+                if (keyValue.Key >= keyEntered)
+                {
+                    if (keyValue.Key < keyCompared)
+                    {
+                        keyCompared = keyValue.Key;
+                    }
+                }
+            }
+            Console.WriteLine("The closest key value to your entered is: " + keyCompared);
 
             //Zavdannya 3
             int[] mas = new int[50];
